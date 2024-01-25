328e946ceb67a6ede9d4a43c28b689af740cce1d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-25 20:37:46 -0300

    refactor: move sign-up routes to /user group

--- src/infra/http/routes/user.ts
+++ src/infra/http/routes/user.ts
@@ -1,26 +1,30 @@
 import Elysia, { t } from "elysia";
 import { servicesPlugin } from "../../elysia/services-plugin";
 
-export const userRoutes = new Elysia().use(servicesPlugin).post(
-  "/sign-up",
-  async ({ body, signUp, set }) => {
-    const user = await signUp.execute(body.name, body.email, body.password);
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    };
-  },
-  {
-    body: t.Object({
-      name: t.String(),
-      email: t.String(),
-      password: t.String(),
-    }),
-    response: t.Object({
-      id: t.String(),
-      name: t.String(),
-      email: t.String(),
-    }),
-  }
-);
+export const userRoutes = new Elysia()
+  .use(servicesPlugin)
+  .group("/user", (app) =>
+    app.post(
+      "/sign-up",
+      async ({ body, signUp, set }) => {
+        const user = await signUp.execute(body.name, body.email, body.password);
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      },
+      {
+        body: t.Object({
+          name: t.String(),
+          email: t.String(),
+          password: t.String(),
+        }),
+        response: t.Object({
+          id: t.String(),
+          name: t.String(),
+          email: t.String(),
+        }),
+      }
+    )
+  );
